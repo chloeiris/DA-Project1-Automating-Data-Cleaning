9b2d0d4f5577a98faa3f0a0ce3f2979348535514
antoanela31 <antoanelamateciuc@gmail.com>
2022-07-14 16:58:22 +0200

    creacion de la tabla datasql

--- 220713 _Proyecto.sql
+++ 220713 _Proyecto.sql
@@ -1,8 +1,31 @@
 -- CREAMOS LA BASE DE DATOS -- 
-
-CREATE SCHEMA IF NOT EXISTS proyecto1;
+CREATE SCHEMA IF NOT EXISTS project1;
 
 -- USAMOS NUESTRA BASE DE DATOS --
+USE project1;
 
-USE proyecto1;
+-- CREAMOS LA TABLA DE DATA SQL---
+CREATE TABLE datasql(
+`index_sql` int not null auto_increment,
+primary key (`index_sql`),
+`q10_part_1` varchar(255),
+`q10_part_2` varchar(255),
+`q10_part_3` varchar(255),
+`q10_part_4` varchar(255),
+`q10_part_5` varchar(255),
+`q10_part_6` varchar(255),
+`q10_part_7` varchar(255),
+`q10_part_8` varchar(255),
+`q10_part_9` varchar(255),
+`q10_part_10` varchar(255),
+`q10_part_11` varchar(255),
+`q10_part_12` varchar(255),
+`q10_part_13` varchar(255),
+`q10_part_14` varchar(255),
+`q10_part_15` varchar(255),
+`q10_part_16` varchar(255),
+`q10_other` varchar(255),
+`d482xta` varchar(255),
+);
+
 
